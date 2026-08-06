ae9bb33a00508536f645889b8c38ac4413d1d7da
Andres <jnataren308@gmail.com>
2026-08-06 15:08:41 -0600

    Archivos conexion a la BD

--- config/conexion.php
+++ config/conexion.php
@@ -5,6 +5,7 @@
     try{
         $conexion = new PDO($dsn, $username, $password);
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Conexión exitosa a la base de datos';
     }catch(PDOException $e){
         echo 'Error de conexión: ' . $e->getMessage();
     }
