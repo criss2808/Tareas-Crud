d0022f213612850653d61cf892a4906b69d20155
Andres <jnataren308@gmail.com>
2026-08-06 14:29:22 -0600

    Archivos de tailwind

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud de Tareas</title>
+    <link rel="stylesheet" href="assets/CSS/SRC/output.css">
+</head>
+<body>
+    <h1 class="uppercase text-2xl font-bold bg-amber-600 text-center">Control de Tareas en PHP</h1>
+</body>
+</html>
